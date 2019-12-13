225631e94c150926feedbea80688d18f6825ebda
yekaterina <yekaterina.batashov@live.biu.ac.il>
2019-12-13 13:07:27 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/flight_sim1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flight_sim1.dir/main.cpp.o"
+  "flight_sim1"
+  "flight_sim1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flight_sim1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/flight_sim1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for flight_sim1.
+# This may be replaced when dependencies are built.
