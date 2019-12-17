47201704ea3976825120aedc5b7da82b9ca1d325
yekaterina <yekaterina.batashov@live.biu.ac.il>
2019-12-17 18:13:32 +0200

    added a variable object to symbol table

--- Parser/SymbolTable.h
+++ Parser/SymbolTable.h
@@ -7,15 +7,29 @@
 
 #include <map>
 #include <mutex>
+#include "Variable.h"
+#include <vector>
+#include "../Lexer/Lexer.h"
+
 
 using namespace std;
 
 class SymbolTable {
+    map<string,string> generic_small={
+            {"airspeed-indicator_indicated-speed-kt",
+             "/instrumentation/airspeed-indicator/indicated-speed-kt"},
+            {"altimeter_indicated-altitude-ft",
+             "/instrumentation/altimeter/indicated-altitude-ft"},
+            {"altimeter_pressure-alt-ft",
+             "/instrumentation/altimeter/pressure-alt-ft"},
+            {"attitude-indicator_indicated-pitch-deg",
+             "/instrumentation/attitude-indicator/indicated-pitch-deg"}
+    };
 public:
-    static map<string, double> symT;
+    static map<string, Variable> symT;
     mutex mutex_lock;
 
-    void addToMap(char *buffer, int size);
+    void addToSimMap(char *buffer, int size);
 
     double getFromMap(string str);
 };
