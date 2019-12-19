574876ff4803221c74fda98f53d2f25c9572c671
elad <eladgarber@gmail.com>
2019-12-19 16:18:54 +0200

    removing redundant print

--- Parser/SleepCommand.cpp
+++ Parser/SleepCommand.cpp
@@ -8,7 +8,6 @@ void SleepCommand::execute() {
     try {
         int sleepDuration = stoi(commandLexer.at(0));
         std::this_thread::sleep_for(std::chrono::milliseconds(sleepDuration));
-        cout << "finish sleep" << endl;
     } catch (...) {
         cerr << "Could not parse 'Sleep' duration" << endl;
     }
